6fee7d95c6f8157a65940f17a4efc1fb08c7a066
Xtarii <carlalvinelias@gmail.com>
2024-07-15 17:27:57 +0200

    Windowless startup

--- src/auto_start.py
+++ src/auto_start.py
@@ -4,14 +4,16 @@ import os
 
 # Checks if Application is in Autostart
 def check_autostart(STARTUP_PATH: str, NAME: str):
-    return os.path.exists(os.path.join(STARTUP_PATH, NAME + ".bat")) # Check if Startup
+    return os.path.exists(os.path.join(STARTUP_PATH, NAME + ".vbs")) # Check if Startup
 
 def add_to_autostart(STARTUP_PATH: str, NAME: str, PATH: str):
     # Check if This application already is in Autostart
     if(not check_autostart(STARTUP_PATH, NAME)):
-        shortcut = os.path.join(STARTUP_PATH, NAME + ".bat")
-
-        content = "@echo off\n" + PATH + "\n" # Manually set this
+        shortcut = os.path.join(STARTUP_PATH, NAME + ".vbs")
+        content = f'''Set WshShell = CreateObject("WScript.Shell")
+        WshShell.Run chr(34) & "{PATH}" & chr(34), 0
+        Set WshShell = Nothing
+        '''
 
         # Creates Shortcut
         with open(shortcut, "w") as file:
